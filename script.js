87d4603d69c2329fb4dd1dfb983fb362064b6eb7
tihi <77891364+JustTizian@users.noreply.github.com>
2025-09-05 09:46:15 +0200

    Use event delegation for delete and toggle

--- script.js
+++ script.js
@@ -1,78 +1,94 @@
-function $(query){
+function $(query) {
     return document.querySelector(query)
 }
 
 const bookContainer = $(".book-container");
 const newBookForm = $(".new-book-form");
 
-let myLibrary = []
+const myLibrary = []
 
-function Book(title, author, pages){
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = false;
+    this.read = read;
     this.id = crypto.randomUUID();
 }
 
-Book.prototype.changeReadStatus = function(){
+Book.prototype.changeReadStatus = function () {
     this.read = !this.read
     displayBooks();
 }
 
-function addBookToLibrary(title, author, pages){
-    myLibrary.push(new Book(title, author, pages));
+function addBookToLibrary(title, author, pages, read) {
+    myLibrary.push(new Book(title, author, pages, read));
     displayBooks();
 }
 
-addBookToLibrary("The Hobbit", "J. R. R. Tolkien", 265);
-addBookToLibrary("Harry Potter", "J. K. Rowling", 322);
-addBookToLibrary("Der Sandmann", "E. T. A. Hoffmann", 150);
-addBookToLibrary("Faust I", "Johann Wolgang von Goethe", 475);
-addBookToLibrary("Faust I", "Johann Wolgang von Goethe", 475);
-
+addBookToLibrary("The Hobbit", "J. R. R. Tolkien", 265, false);
+addBookToLibrary("Harry Potter", "J. K. Rowling", 322, false);
+addBookToLibrary("Der Sandmann", "E. T. A. Hoffmann", 150, false);
+addBookToLibrary("Faust I", "Johann Wolgang von Goethe", 475, false);
+addBookToLibrary("Faust II", "Johann Wolgang von Goethe", 200, false);
 
 console.table(myLibrary)
 
-function displayBooks(){
-    bookContainer.innerHTML = ""
+function displayBooks() {
+    bookContainer.replaceChildren();
     myLibrary.forEach(book => {
         const bookCardDiv = document.createElement("div");
         bookCardDiv.classList.add("book-card");
-        //bookCardDiv.setAttribute("data-id", book.id);
-        
+        bookCardDiv.setAttribute("data-id", book.id);
+
         const titleDisplay = document.createElement('h2');
         const authorDisplay = document.createElement('h3');
         const pagesDisplay = document.createElement('h4');
         const readDisplay = document.createElement('h4');
 
-        
         titleDisplay.textContent = book.title;
         authorDisplay.textContent = book.author;
         pagesDisplay.textContent = `${book.pages} pages`;
         readDisplay.textContent = book.read;
 
         const deleteButton = document.createElement("button");
-        deleteButton.addEventListener("click", () => {
-            deleteBook(book.id);
-        })
-        deleteButton.textContent = "Delete"
+        deleteButton.classList.add("delete-btn");
+        deleteButton.textContent = "Delete";
 
         const toggleButton = document.createElement("button");
-        toggleButton.addEventListener("click", () => {
-            book.changeReadStatus()
-        })
-        toggleButton.textContent = "Toggle"
-        
+        toggleButton.classList.add("toggle-btn");
+        toggleButton.textContent = "Toggle";
+
         bookCardDiv.append(titleDisplay, authorDisplay, pagesDisplay, readDisplay, deleteButton, toggleButton)
         bookContainer.appendChild(bookCardDiv);
     })
 }
 
-function deleteBook(id){
-    myLibrary = myLibrary.filter(book => book.id != id);
+function deleteBook(id) {
+    const index = myLibrary.findIndex(book => book.id === id)
+    myLibrary.splice(index, 1)
     displayBooks();
 }
+
+function handleReadStatusToggle(id){
+    const book = myLibrary.find(book => book.id === id)
+    console.log(book)
+    if(book){
+        book.changeReadStatus();
+        displayBooks();
+    }
+}
+
+bookContainer.addEventListener("click", (event) => {
+    const bookCard = event.target.closest(".book-card");
+    
+    if(!bookCard) return;
+    
+    const id = bookCard.dataset.id;
+    
+    if(event.target.matches(".delete-btn")) deleteBook(id)
+    if(event.target.matches(".toggle-btn")) handleReadStatusToggle(id)
+
+})
 
 displayBooks();
 
@@ -82,5 +98,6 @@ newBookForm.addEventListener("submit", (event) => {
     const title = data["title"].value;
     const author = data["author"].value;
     const pages = data["pages"].value;
-    addBookToLibrary(title, author, pages)
+    const read = data["read"].checked;
+    addBookToLibrary(title, author, pages, read)
 })
